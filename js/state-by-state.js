6667ef85481dbb91a78285b19d4aba4bf7e3a986
jgyou-bu <jgyou@bu.edu>
2016-12-02 00:32:51 -0500

    rates on state page now change with slider input

--- js/state-by-state.js
+++ js/state-by-state.js
@@ -1,11 +1,42 @@
 // slider for years on state-by-state page
 
-var slider = new Slider('#year', {
+$('#year').slider({
   formatter: function (value) {
     return value;
   }
 });
 
+
+// sample rates
+var firearm_suicides = {
+  "2010": 2.9,
+  "2011": 3.4,
+  "2012": 4.1,
+  "2013": 3.3,
+  "2014": 4.2,
+  "2015": 4.3,
+  "2016": 5.1
+};
+
+var firearm_homicides = {
+  "2010": 3.3,
+  "2011": 2.9,
+  "2012": 3.1,
+  "2013": 3.3,
+  "2014": 3.5,
+  "2015": 3.2,
+  "2016": 3.3
+};
+
+
+// rates displayed change based on year
+$("#year").on("slide", function(slideEvt) {
+  $("#firearm-suicides").text(firearm_suicides[slideEvt.value]);
+  $("#firearm-homicides").text(firearm_homicides[slideEvt.value]);
+});
+
+
+// load dropdown menu of states
 $.getJSON("js/states-list.json", function (data) {
   var states_arr = data['states'];
   var stateDropdown = $("#stateDropdown");
@@ -14,15 +45,3 @@ $.getJSON("js/states-list.json", function (data) {
     $(stateDropdown).append("<button class='dropdown-item' type='button'>" + state + "</button>");
   }
 });
-
-// var firearm_suicides = {
-//   2012: 4.1,
-//   2013: 3.3,
-//   2014: 5.2,
-//   2015: 5.3,
-//   2016: 5.1
-// };
-//
-// slider.on("slide", function(slideEvt) {
-//   $("#firearm-suicides").text(firearm_suicides[slideEvt.value]);
-// });
