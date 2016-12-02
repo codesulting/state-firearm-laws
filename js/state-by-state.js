79a18ca9b052c1d83e5f3f411bedd4771d37da66
jgyou-bu <jgyou@bu.edu>
2016-12-01 23:53:18 -0500

    included dropdown list of states

--- js/state-by-state.js
+++ js/state-by-state.js
@@ -5,3 +5,24 @@ var slider = new Slider('#year', {
     return value;
   }
 });
+
+$.getJSON("js/states-list.json", function (data) {
+  var states_arr = data['states'];
+  var stateDropdown = $("#stateDropdown");
+  for (var i = 0; i < states_arr.length; i++) {
+    var state = states_arr[i];
+    $(stateDropdown).append("<button class='dropdown-item' type='button'>" + state + "</button>");
+  }
+});
+
+// var firearm_suicides = {
+//   2012: 4.1,
+//   2013: 3.3,
+//   2014: 5.2,
+//   2015: 5.3,
+//   2016: 5.1
+// };
+//
+// slider.on("slide", function(slideEvt) {
+//   $("#firearm-suicides").text(firearm_suicides[slideEvt.value]);
+// });
