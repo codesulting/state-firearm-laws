5b7cc53f0361737d3967720d2e6b03f1a17bf5e3
jgyou-bu <jgyou@bu.edu>
2017-02-26 23:43:04 -0500

    updated conversion scripts to add to raw-data.json

--- py/json_conversion.py
+++ py/json_conversion.py
@@ -1,26 +1,83 @@
+'''
+
+*** note: the term "provision" and "variable" are used interchangeably
+
+glossary_conversion()
+
+uses:
+- codebook.csv
+
+modifies/creates:
+- glossary.json, to be used with glossary.html to show table of variables and corresponding definitions
+- raw_data_table.json, to be used with table.html to show overall table of data for download
+_____
+
+history_conversion()
+
+uses:
+- current-repealed-list.csv
+
+modifies/creates:
+- history/<state-name>.json files, to be used with state-by-state.html
+_________
+
+rates_conversion()
+
+uses:
+- state-rates.csv
+
+modifies/creates
+- history/<state-name>.json files, to be used with state-by-state.html
+_________
+
+raw_data_conversion()
+
+uses:
+- states-list.json
+- firearms.database.csv (columns for state, year, and each variable)
+
+modifies/creates:
+- raw-data.json
+
+
+'''
+
 import json, csv
 import sys, os
 
 
-# for converting codebook file to glossary
+# for converting codebook file to glossary, as well as adding variable data to raw_data table
 def glossary_conversion(csvreader):
+    cwd = os.getcwd()
     rows_arr = []
+    rows_arr2 = []
     categories = set()
     subcategories = set()
     exported_headers = ["code", "category", "subcategory", "description", "variable"]
-    exported = dict()
+
     for row in csvreader:
         entry = {e: row[e] for e in exported_headers}
+        entry2 = dict()
+        entry2["category"] = row["category"]
+        entry2["variable"] = row["variable"]
+        entry2["subcategory"] = row["subcategory"]
         # each row in table as a dictionary
         rows_arr.append(entry)
+        rows_arr2.append(entry2)
 
         categories.add(row["category"])
         subcategories.add(row["subcategory"])
 
     exported = {"rows": rows_arr, "categories": list(categories), "subcategories": list(subcategories)}
 
-    with open("glossary.json", "w") as outfile:
+    # export glossary file
+    with open(os.path.join("js", "glossary.json"), "w") as outfile:
         json.dump(exported, outfile, sort_keys=True, indent=4)
+    # add to raw-data-table file
+    with open(os.path.join(cwd, "js", "raw-data.json"),'w') as outfile2:
+        exported = {"provisions": rows_arr2, "categories": list(categories), "subcategories": list(subcategories)}
+        json.dump(exported, outfile2, sort_keys=True, indent=4)
+
 
 
 # for converting current/repealed list to a state-based json file
@@ -56,8 +113,9 @@ def history_conversion(csvreader):
 
     # create output json file for each state
     for state in states_list:
-        with open(state + ".json", "w") as outfile:
+        with open(os.path.join("js", "history", state + ".json"), "w") as outfile:
             json.dump({"data": states_dict[state]}, outfile, sort_keys=True, indent=4)
+
 
 
 # converts csv with homicide, suicide rates
@@ -97,6 +155,7 @@ def rates_conversion(csvreader):
                 json.dump(current_state_dict, updated_state_file, sort_keys=True, indent=4)
 
 
+
 # converts raw data table (csv format) to json
 def raw_data_conversion(csvreader):
     exported = dict()
@@ -105,8 +164,24 @@ def raw_data_conversion(csvreader):
     for row in csvreader:
         exported["rows"].append({i: row[i] for i in exported["columns"]})
 
-    with open("raw-data.json", "w") as outfile:
-        json.dump(exported, outfile, sort_keys=True, indent=4)
+    cwd = os.getcwd()
+
+
+    # add existing raw_data info
+    with open(os.path.join(cwd, "js", "raw-data.json"), "r") as inputfile:
+        raw_data = json.load(inputfile)
+        raw_data_export = raw_data.copy()
+        raw_data_export.update(exported)
+
+        # add list of states to raw data
+        with open(os.path.join(cwd, "js", "states-list.json"), "r") as inputfile2:
+            states_list = json.load(inputfile2)
+            raw_data_export.update(states_list)
+
+        # update json file
+        with open( os.path.join("js", "raw-data.json"), "w") as outfile:
+            json.dump(raw_data_export, outfile, sort_keys=True, indent=4)
+
 
 
 with open(sys.argv[1], 'r') as csvfile:
