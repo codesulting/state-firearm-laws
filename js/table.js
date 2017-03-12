fadbf5d41e7e9d8fa1075b6f847fbb33c152697a
jgyou-bu <jgyou@bu.edu>
2017-03-12 17:34:03 -0400

    included xls and csv download

--- js/table.js
+++ js/table.js
@@ -63,7 +63,7 @@ function generateInitialTable(columns, rows) {
 
 
   // generate column names for DataTable
-  colNamesDT = []
+  var colNamesDT = []
   for (var i = 0; i < columns.length; i++) {
     colNamesDT.push({"name": columns[i]});
   }
@@ -91,7 +91,7 @@ function generateInitialTable(columns, rows) {
       for (var i = 0; i < statesSelected.length; i++) {
         stateSearchSeq += statesSelected[i];
         if (i < statesSelected.length - 1) {
-          stateSearchSeq += "\|";
+          stateSearchSeq += "|";
         }
       }
     } else {
@@ -131,9 +131,9 @@ function generateInitialTable(columns, rows) {
 
     // redraw columns afterwards
 
-    // rawTable.column("state:name").search(stateSearchSeq, true, false);
+    rawTable.column(0).search(stateSearchSeq, true, false, true).draw();
     // rawTable.columns.adjust().draw();
-    rawTable.columns.adjust().draw(false);
+    // rawTable.columns.adjust().draw(false);
 
 
     // draw
@@ -143,14 +143,25 @@ function generateInitialTable(columns, rows) {
 
 // events for download buttons
 
-//$('#csv_button').click(function() {
-//   var csvData = arrToCSV(getTableData());
-//   saveAs(new Blob([csvData], {type: "text/plain;charset=utf-8"}), "raw_data.csv");
-// );
+$('#csv_button').click(function() {
+  var csvData = arrToCSV(getTableData());
+  saveAs(new Blob([csvData], {type: "data:text/csv;charset=utf-8"}), "raw_data.csv");
+});
 
 $('#txt_button').click(function () {
   var txtData = arrToTXT(getTableData());
   saveAs(new Blob([txtData], {type: "text/plain;charset=utf-8"}), "data.txt");
+});
+
+$('#xls_button').click(function () {
+  var ws = sheet_from_array_of_arrays(getTableData());
+  var wb = new Workbook();
+  var ws_name = "data"
+  wb.SheetNames.push(ws_name);
+  wb.Sheets[ws_name] = ws;
+  var wbout = XLSX.write(wb, {bookType: 'biff2', bookSST: false, type: 'binary'});
+  var fname = "data.xls";
+  saveAs(new Blob([s2ab(wbout)], {type: "application/octet-stream"}), fname);
 });
 
 
@@ -218,4 +229,64 @@ function getRowData(data) {
     finalList.push(data.eq(c).text());
   }
   return finalList;
+}
+
+
+// from xlsx-js package's demo
+
+function datenum(v/*:Date*/, date1904/*:?boolean*/)/*:number*/ {
+  var epoch = v.getTime();
+  if (date1904) epoch += 1462 * 24 * 60 * 60 * 1000;
+  return (epoch + 2209161600000) / (24 * 60 * 60 * 1000);
+}
+
+// from xlsx-js package's demo
+function sheet_from_array_of_arrays(data, opts) {
+  var ws = {};
+  var range = {s: {c: 10000000, r: 10000000}, e: {c: 0, r: 0}};
+  for (var R = 0; R != data.length; ++R) {
+    for (var C = 0; C != data[R].length; ++C) {
+      if (range.s.r > R) range.s.r = R;
+      if (range.s.c > C) range.s.c = C;
+      if (range.e.r < R) range.e.r = R;
+      if (range.e.c < C) range.e.c = C;
+      var cell = {v: data[R][C]};
+      if (cell.v == null) continue;
+      var cell_ref = XLSX.utils.encode_cell({c: C, r: R});
+
+      if (typeof cell.v === 'number') cell.t = 'n';
+      else if (typeof cell.v === 'boolean') cell.t = 'b';
+      else if (cell.v instanceof Date) {
+        cell.t = 'n';
+        cell.z = XLSX.SSF._table[14];
+        cell.v = datenum(cell.v);
+      }
+      else cell.t = 's';
+
+      ws[cell_ref] = cell;
+    }
+  }
+  if (range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+  return ws;
+}
+
+// from xlsx-js package's demo
+function Workbook() {
+  if (!(this instanceof Workbook)) return new Workbook();
+  this.SheetNames = [];
+  this.Sheets = {};
+}
+
+// from xlsx-js package's demo
+function s2ab(s) {
+  if (typeof ArrayBuffer !== 'undefined') {
+    var buf = new ArrayBuffer(s.length);
+    var view = new Uint8Array(buf);
+    for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+    return buf;
+  } else {
+    var buf = new Array(s.length);
+    for (var i = 0; i != s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
+    return buf;
+  }
 }
