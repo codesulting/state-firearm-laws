753cd79b57451b68b89159d97da90c63292fee18
jgyou-bu <jgyou@bu.edu>
2017-03-17 18:03:30 -0400

    fixed scrolling through table by removing iframe
    addresses a problem listed in #2

--- js/law-data.js
+++ js/law-data.js
@@ -13,19 +13,3 @@ vizElement.style.minHeight = '629px';
 var scriptElement = document.createElement('script');
 scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
 vizElement.parentNode.insertBefore(scriptElement, vizElement);
-
-
-
-/* for displaying Tableau table */
-var divElement2 = document.getElementById('viz1480296125463');
-var vizElement2 = divElement2.getElementsByTagName('object')[0];
-vizElement2.style.minWidth = '100%';
-/*
-vizElement.style.minWidth='424px';
- vizElement.style.maxWidth='100%';
- */
-vizElement2.style.minHeight='629px';
-//vizElement.style.maxHeight=(divElement.offsetWidth*0.75)+'px';
-var scriptElement2 = document.createElement('script');
-scriptElement2.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-vizElement2.parentNode.insertBefore(scriptElement2, vizElement2);
