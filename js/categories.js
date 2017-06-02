6ad0b11560429f07e111dd1539b1e35a96902966
qmei1123 <qmei@bu.edu>
2017-06-02 10:38:58 -0400

    Redesign Law Categories page.

--- js/categories.js
+++ js/categories.js
@@ -1,48 +1,6 @@
-var data = $.getJSON("js/glossary.json", function (obj) {
-
-  // Retrieve data.
-  data = obj['rows'];
-
-  // Obtain and alphabetize list of categories.
-  var categories = obj['categories'];
-  categories.sort();
-
-  // Create dropdown for categories.
-  var categoriesContent = "<select class='selectpicker' id='category_menu'>";
-
-  for (var i = 0; i < categories.length; i++) {
-    categoriesContent += "<option>" + categories[i] + "</option>";
-  }
-
-  categoriesContent += "</select>";
-
-  $('#category_menu_div').append(categoriesContent);
-
-
-  // Set default category explanation/div to show.
-  $('#category_menu').selectpicker('val', 'Ammunition regulations');
-  updateDiv();
-
-  // Show appropriate div based on chosen dropdown.
-  // Requires that div heading exactly matches categories as stored in glossary.json (not case-sensitive).
-
-  $('#category_menu').on('changed.bs.select', function () {
-    updateDiv();
-  });
-
-
+$('.list-group-div').hide();
+$('.list-group-item').click(function() {
+    $(this).next().slideToggle();
 });
 
-// Shows div based on dropdown option chosen. Hides remaining divs.
-function updateDiv() {
-  $('#category_text_div').children().css("display", "none");
-  var categoryChosen = $('#category_menu').val();
-  $('#category_text_div').find("div").each(function(index) {
-    // Matches header to dropdown selection.
-    if ($(this).children("h3:first").text().toLowerCase() === categoryChosen.toLowerCase()) {
-      $(this).show();
-    } else {
-      $(this).hide();
-    }
-  })
-}
+
